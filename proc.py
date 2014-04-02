f246b7610cb7a22b7c6dfe3e676c25f80fd7d94b
Austin Sims <sims4@purdue.edu>
2014-04-01 23:19:24 -0400

    added proc.py

---
+++ proc.py
@@ -0,0 +1,22 @@
+import	re
+import	sys
+
+infile	= open(sys.argv[1],'r')
+s = infile.read()
+infile.close()
+s = s.replace('"','')
+s = re.sub(r'[ ]{2,}','\t',s)
+
+out = open(sys.argv[1], 'w')
+lines = s.split('\n')
+lines = filter(lambda a: a != '',lines)
+i = 0
+for line in lines:
+    out.write('"' + line + r'\n"')
+    if i == len(lines) - 1:
+	out.write(',')
+    else:
+        out.write('+')
+    out.write('\n')
+    i=i+1
+out.close()
